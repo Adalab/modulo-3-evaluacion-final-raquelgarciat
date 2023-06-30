c8ac3bb36934f9cee14230b730b146f6542d2853
raquelgarciat <raquelgarciat@gmail.com>
2023-06-30 18:14:48 +0200

    add fetch service

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,51 @@
+// IMPORTS
+
+import { useState, useEffect } from 'react';
+import getDataFromApi from '../services/api';
 import '../styles/App.css';
 
+// FUNCIÓN DEL COMPONENTE
+
 function App() {
+  // SECCIÓN DE VARIABLES DE ESTADO
+
+  const [charList, setCharList] = useState([]);
+
+  // SECCIÓN DE CÓDIGO CUANDO CARGA LA PÁGINA
+
+  useEffect(() => {
+  
+    getDataFromApi().then((cleanData) => {
+      setCharList(cleanData);
+      console.log(cleanData);
+    });
+
+  }, []);
+
+  // SECCION HTML
+
   return (
-    <div>
-      <h1>Hola mundo! (^¬^)/♥</h1>
+    <div className='page'>
+      <header className='header'>
+        <h1 className='header_title'>Rick & Morty</h1>
+      </header>
+      <main className='main'>
+        <form className='filters'>
+          <h2 className='filters__title title--medium'>Filtrar por...</h2>
+        </form>
+        <section className='contacts'>
+          <h2 className='contacts__title title--medium'>Lista de personajes</h2>
+          <ul className='cards'>
+            <li className='card'></li>
+            <li className='card'></li>
+            <li className='card'></li>
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
+
+// EXPORT DEL COMPONENTE
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+const getDataFromApi = () => {
+    return fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(data => {
+      const cleanData = data.results.map((objectAPI) => {
+        return {
+            id: objectAPI.id,
+            name: objectAPI.name,
+            picture: objectAPI.image,
+            status: objectAPI.status,
+            species: objectAPI.species,
+            episodes: objectAPI.episode.length,
+            planet: objectAPI.location.name,
+        }
+      });
+  
+      return cleanData;
+    })
+  
+  };
+  
+  export default getDataFromApi;
